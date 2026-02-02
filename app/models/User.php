80a1296cf5db6089f6a9d13ad22d0dbd0aebf849
Nate Ilunga <ilunganathan110@gmail.com>
2026-02-02 20:15:29 +0200

    Receives and inserts validated data into DB

---
+++ app/models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+class User {
+    private $db;
+
+    public function __construct() {                     //function runs automatically when an object is created
+        $this->db = (new Database())->connect();        //creates a new object from Database class, calls the connect function from Database class, a db function is returned. The connection is stored inside the User object to be reused
+    }
+
+    public function create($CleanUserData) {    //$CleanUserData is the validated data from AuthController
+        try {                                           //try because it might fail
+            $query = "
+                INSERT INTO users (name, email, password)
+                VALUES (:name, :email, :password)                 
+            ";                                            //SQL instructions, insert a new row in the users table with name, email and password as placeholders for now not real values yet
+
+            $stmt = $this->db->prepare($query);    //prepares the db connection. Prevents SQL injection
+            $stmt->execute([                     // runs the query so now the db stores the actual name,email and password values
+                ':name'     => $CleanUserData['name'],
+                ':email'    => $CleanUserData['email'],
+                ':password' => $CleanUserData['password']
+            ]);
+
+            return "success";                                  //returns message if everything worked
+
+        } catch (PDOException $e) {                           //if inserting fails. $e is the error message from the db
+            
+            if ($e->getCode() == 23000) {                     // get the code from the db and if it is 2300, the value is a duplicate
+                return "duplicate";                            // user email already exists
+            }
+            return "Unknown error occured";                                   // if the error is NOT a duplicate, something else went wrong
+        }
+    }
+}

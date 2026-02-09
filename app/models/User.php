d75f81b889de81ebf4c164f1d78ef032622db87c
Nate Ilunga <ilunganathan110@gmail.com>
2026-02-09 20:47:44 +0200

    Receives and inserts validated data into DB. Added role data field that was left out

--- app/models/User.php
+++ app/models/User.php
@@ -7,20 +7,21 @@ class User {
 
     public function __construct() {                     //function runs automatically when an object is created
         $this->db = (new Database())->connect();        //creates a new object from Database class, calls the connect function from Database class, a db function is returned. The connection is stored inside the User object to be reused
-    }
+    }                                                   //creates a connection even before the clean data from Authcontroller is saved into a db
 
     public function create($CleanUserData) {    //$CleanUserData is the validated data from AuthController
         try {                                           //try because it might fail
             $query = "
-                INSERT INTO users (name, email, password)
-                VALUES (:name, :email, :password)                 
+                INSERT INTO users (name, email, password, role)
+                VALUES (:name, :email, :password, :role)                 
             ";                                            //SQL instructions, insert a new row in the users table with name, email and password as placeholders for now not real values yet
 
             $stmt = $this->db->prepare($query);    //prepares the db connection. Prevents SQL injection
             $stmt->execute([                     // runs the query so now the db stores the actual name,email and password values
-                ':name'     => $CleanUserData['name'],
-                ':email'    => $CleanUserData['email'],
-                ':password' => $CleanUserData['password']
+                ":name"     => $CleanUserData["name"],
+                ":email"    => $CleanUserData["email"],
+                ":password" => $CleanUserData["password"],
+                ":role" => $CleanUserData["role"]
             ]);
 
             return "success";                                  //returns message if everything worked
@@ -33,4 +34,5 @@ class User {
             return "Unknown error occured";                                   // if the error is NOT a duplicate, something else went wrong
         }
     }
+        
 }
